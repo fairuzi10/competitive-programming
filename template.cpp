732cf5c77acb27b28afe4a7c57ab4d2095ab505b
M Fairuzi T <mfairuzit@gmail.com>
2017-09-21 15:37:50 +0700

    init

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define INF 1000000000
+#define MOD 1000000007
+#define MAXN 100005
+#define lint long long
+#define trace(x) cout<<#x<<" = "<<x<<'\n'
+#define trace2(x, y) cout<<#x<<" = "<<x<<" "<<#y<<" = "<<y<<'\n'
+#define tracea(arr, i) cout<<#arr<<"["<<i<<"]"<<" = "<<arr[i]<<'\n'
+#define print(arr, n) for (int iDx = 0; iDx < (n); iDx++) cout << arr[iDx] << " \n"[iDx == (n)-1]
+#define sz(x) (int)(x).size()
+using namespace std;
+struct ii {
+	int a, b;
+	bool operator < (ii o) const { return make_tuple(a, b) < make_tuple(o.a, o.b); }
+};
+struct iii {
+	int a, b, c;
+	bool operator < (iii o) const { return make_tuple(a, b, c) < make_tuple(o.a, o.b, o.c); }
+};
+int TC, N, M, K, Q, x, y, z;
+
+
+
+int main() {
+	#ifdef FAIRUZI10
+	freopen("/home/fairuzi/Documents/CP/input.in", "r", stdin);
+	#endif
+	
+	
+	
+	return 0;
+}
