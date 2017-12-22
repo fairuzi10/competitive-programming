16a32683c610687c34ccf124ad5d9bbb8444d4f5
M Fairuzi T <mfairuzit@gmail.com>
2017-12-22 14:46:00 +0700

    minor change suffix array implementation

--- code/string/suffix-array-cycle.cpp
+++ code/string/suffix-array-cycle.cpp
@@ -12,27 +12,29 @@ int rentang;
 int cnt[MAXN], sa[MAXN], tsa[MAXN], ra[MAXN], tra[MAXN];
 vector<int> urutan;
 
-void radix(int step) {
+void radix(int k) {
 	memset(cnt, 0, sizeof(cnt));
 	int maxi = max(300, N);
-	for (int i = 0; i < N; i++) cnt[ra[(sa[i]+step)%N]]++;
+	for (int i = 0; i < N; i++) cnt[ra[min(sa[i]+k, sa[i]+rentang-k)%N]]++;
 	for (int i = 1; i < maxi; i++) cnt[i] += cnt[i-1];
-	for (int i = N-1; i >= 0; i--) tsa[--cnt[ra[(sa[i]+step)%N]]] = sa[i];
+	for (int i = N-1; i >= 0; i--) tsa[--cnt[ra[min(sa[i]+k, sa[i]+rentang-k)%N]]] = sa[i];
 	for (int i = 0; i < N; i++) sa[i] = tsa[i];
 }
 
-void compute_sa(int rem) {
-	int step = rem/2;
-	if (rem >= 2) compute_sa(step + rem%2);
-	radix(step);
-	radix(0);
-	tra[sa[0]] = 0;
-	for (int i = 1; i < N; i++) {
-		tra[sa[i]] = ra[sa[i]] == ra[sa[i-1]] && ra[(sa[i]+step)%N] == ra[(sa[i-1]+step)%N]?
-			tra[sa[i-1]]: tra[sa[i-1]]+1;
-	}
-	for (int i = 0; i < N; i++) ra[i] = tra[i];
-}
+void compute_sa() {
+	for (int i = 0; i < N; i++) ra[i] = str[i];
+	for (int i = 0; i < N; i++) sa[i] = i;
+	for (int k = 1; k < rentang; k <<= 1) {
+		radix(k);
+		radix(0);
+		tra[sa[0]] = 0;
+		for (int i = 1; i < N; i++)
+			tra[sa[i]] = ra[sa[i]] == ra[sa[i-1]] && 
+				ra[min(sa[i]+k, sa[i]+rentang-k)%N] == ra[min(sa[i-1]+k, sa[i-1]+rentang-k)%N] ? 
+				tra[sa[i-1]]: tra[sa[i-1]]+1;
+		for (int i = 0; i < N; i++) ra[i] = tra[i];
+		if (ra[sa[N-1]] == N-1) break;
+}}
 
 bool is_valid(int m) {
 	for (int j = 0; j < rentang; j++) {
@@ -50,16 +52,11 @@ int main() {
 	cin >> N >> K;
 	cin >> str;
 	
-	for (int i = 0; i < N; i++) {
-		ra[i] = str[i];
-		sa[i] = i;
-	}
 	rentang = (N+K-1)/K;
-	compute_sa(rentang);
+	compute_sa();
 	for (int i = 0; i < N; i++) {
 		urutan.push_back(sa[i]);
 	}
-	
 	int l = 0, r = N;
 	while (l < r) {
 		int m = (l+r)/2;
--- code/string/suffix-array-lcp.cpp
+++ code/string/suffix-array-lcp.cpp
@@ -11,25 +11,25 @@ char str[MAXN];
 int lcp[MAXN], cnt[MAXN], ra[MAXN], tra[MAXN], sa[MAXN], tsa[MAXN];
 lint ans[30];
 
-void radix(int step) {
+void radix(int k) {
 	int maxi = max(300, N);
 	memset(cnt, 0, sizeof(cnt));
-	for (int i = 0; i < N; i++) cnt[sa[i]+step < N? ra[sa[i]+step]: 0]++;
+	for (int i = 0; i < N; i++) cnt[sa[i]+k < N? ra[sa[i]+k]: 0]++;
 	for (int i = 0; i < maxi; i++) cnt[i] += cnt[i-1];
-	for (int i = N-1; i >= 0; i--) tsa[--cnt[sa[i]+step < N? ra[sa[i]+step]: 0]] = sa[i];
+	for (int i = N-1; i >= 0; i--) tsa[--cnt[sa[i]+k < N? ra[sa[i]+k]: 0]] = sa[i];
 	for (int i = 0; i < N; i++) sa[i] = tsa[i];
 }
 
 void compute_sa() {
 	for (int i = 0; i < N; i++) ra[i] = str[i];
 	for (int i = 0; i < N; i++) sa[i] = i;
-	for (int step = 1; step < N; step <<= 1) {
-		radix(step);
+	for (int k = 1; k < N; k <<= 1) {
+		radix(k);
 		radix(0);
 		tra[sa[0]] = 0;
 		for (int i = 1; i < N; i++)
 			tra[sa[i]] = ra[sa[i]] == ra[sa[i-1]] && 
-				ra[sa[i]+step] == ra[sa[i-1]+step]? tra[sa[i-1]]: tra[sa[i-1]]+1;
+				ra[sa[i]+k] == ra[sa[i-1]+k]? tra[sa[i-1]]: tra[sa[i-1]]+1;
 		for (int i = 0; i < N; i++) ra[i] = tra[i];
 		if (ra[sa[N-1]] == N-1) break;
 }}
@@ -44,8 +44,7 @@ void compute_lcp() {
 		cur_lcp = max(0, cur_lcp-1);
 		while (str[i+cur_lcp] == str[sa[ra[i]-1]+cur_lcp]) cur_lcp++;
 		lcp[i] = cur_lcp;
-	}
-}
+}}
 
 int main() {
 	scanf("%s", str);
